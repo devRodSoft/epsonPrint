56c01bd73cba40f54b4d23c235c487076f6faad4
Rod Rodriguez <dev.rodsoft@gmail.com>
2020-05-26 16:52:45 -0500

    check img print wrong

--- index.php
+++ index.php
@@ -20,192 +20,130 @@ use Mike42\Escpos\EscposImage;
 *
 * Use a WindowsPrintConnector with the share name to print.
 *
-* @property string $pelicula
-* @property string $clasificacion
-* @property string $duracion
-* @property string $seat
-* @property string $idioma
-* @property string $fecha
-* @property string $boleto
-* @property string $codigo
-* @property string $sala
-* @property string $horario
+* @property string $prodcutos
+* @property string $total
+* @property string $descuento
 */
 date_default_timezone_set('america/mexico_city');
 
 class Ticket  {
-
-	public $pelicula;
-	public $clasificacion;
-	public $duracion;
-	public $seat;
-	public $idioma;
-	public $fecha;
-	public $codigo;
-	public $sala;
-	public $hora;
-	public $user;
-	public $typoBoleto;
-	public $precio; 
-	public $reImprecion;
-
 	
-	public function printTicket () {
+	public function printTicket ($data) {
 
-		//Set the windows connector and connect to the printer, this have to be shared! 
-		$connector = new WindowsPrintConnector('cine-1');
-		$printer = new Printer($connector);
+		try {
+			// Enter the share name for your USB printer here
+			$connector = null;
+			$connector = new WindowsPrintConnector("printer");
 
-		# Vamos a alinear al centro lo próximo que imprimamos
-		$printer->setJustification(Printer::JUSTIFY_CENTER);
-		
-		/*
-			Try to load and print the logo image
-		*/
-		try{
-			$logo = EscposImage::load("logo.png", false);
-			$printer->bitImage($logo);
-		}   catch   (Exception $e)  {/*Logger here*/}
-	
-		/*
-			Print movie name
-		*/
-		//$printer->text("". "\n");
-		$printer->setTextSize(2, 2);
-		$printer->setEmphasis(true);
-		$printer->text($this->pelicula. "\n");
-		$printer->setEmphasis(false);		
+			$printer = new Printer($connector);
 
-		/* 
-			print sala, time and seat
-		*/
-		$printer->setEmphasis(true);
-
-		$printer->text("". "\n");		
-		$printer->setTextSize(2, 2);
-		$printer->text($this->sala. " | " . $this->horario  .  "\n");
-		$printer->text("" . "\n");
-		//$printer->text($this->seat . "\n");
-		$printer->text($this->seat);
-
-		$printer->setEmphasis(false);
-		/*
-			print Clasification, time and lenaguaje
-		*/ 
-
-		$printer->setTextSize(1, 1);
-		$printer->text("" . "\n");
-		$printer->text($this->clasificacion . " | " . $this->duracion  . " " .  "min" . " | " . $this->idioma . "\n");
-		$printer->text($this->fecha . "\n");
-
-		//$printer->text("". "\n");
-		$printer->text("Vendedor: " . $this->user . " | " . $this->typoBoleto . " " . $this->precio . "\n");	
-		$printer->text("Codigo: " . $this->codigo . "\n");
-		//$printer->text("". "\n");
+			# Vamos a alinear al centro lo próximo que imprimamos
+			$printer->setJustification(Printer::JUSTIFY_CENTER);
 			
+			/*
+				Try to load and print the logo image
+			*/
+			try{
+				$logo = EscposImage::load("logo.png", false);
+				$printer->bitImage($logo);
+			}   catch   (Exception $e)  {/*Logger here*/}
 
-		/*
+			$printer->feed(1);
+			$printer->setTextSize(1, 1);
+			$printer->text("MAFRA Boutique". "\n");			
+			$printer->text("Hernández #66 Arandas, Jal". "\n");
+			$printer->text("Tel: 3481953503". "\n");
+			$printer->text($this->getFormatDate(). "\n");
+			$printer->setTextSize(1, 1);
+
+			/*
 			Print separator
-		*/
-		$printer->setTextSize(2, 2);
-		$printer->setEmphasis(true);
-		$printer->text("------------------------". "\n");
-		$printer->setEmphasis(false);
-		$printer->setTextSize(1, 1);
-		/* 
-			print sala, time and seat to cut 
-		*/ 
+			*/
 
-		$printer->text("". "\n");
-		$printer->setTextSize(2, 1);
-		$printer->text($this->pelicula. "\n");
-		$printer->setTextSize(1, 2);
-		$printer->text($this->sala. " | " . $this->horario  . " | " . $this->seat . "\n");
-		
-		/*
-			print the movie date
-		*/ 
-		$printer->setTextSize(1, 1);
-		$printer->text("". "\n");
-		$printer->text($this->fecha . "\n");
-		//$printer->text("". "\n");
-		$printer->text("Vendedor: " . $this->user . " | " . $this->typoBoleto . " " . $this->precio . "\n");	
-		$printer->text("Codigo: " . $this->codigo . "\n");
-		$printer->text("". "\n");
+			$printer->setJustification(Printer::JUSTIFY_LEFT);
+			$printer->setTextSize(2, 2);
+			$printer->setEmphasis(true);
+			$printer->text("------------------------". "\n");
+			$printer->setEmphasis(false);
+			$printer->setTextSize(1, 1);
+			$printer->feed(1);
 
-		if ($this->reImprecion) {
-			$printer->text("Boleto Reimpreso" ."\n");
-			//$printer->text("". "\n");
-		}
-		
-		/*Alimentamos el papel 3 veces*/
-		//$printer->feed(5);
-		
-		/*
-			Cortamos el papel. Si nuestra impresora
-			no tiene soporte para ello, no generará
-			ningún error
-		*/
-		$printer->cut();
-		
-		/*
-			Por medio de la impresora mandamos un pulso.
-			Esto es útil cuando la tenemos conectada
-			por ejemplo a un cajón
-		*/
-		//$printer->pulse();
-		
-		/*
-			Para imprimir realmente, tenemos que "cerrar"
-			la conexión con la impresora. Recuerda incluir esto al final de todos los archivos
-		*/
-		$printer->close();
-	}
 
-	public function  getTicketData($seats) {
 
-		$this->pelicula 	 = $seats['pelicula'];
-		$this->clasificacion = $seats['clasificacion'];
-		$this->duracion      = $seats['duracion'];
-		$this->idioma        = $seats['idioma'];
-		$this->reImprecion   = $seats['reImprecion'];
-		$this->fecha         = $this->getFormatDate($seats['fecha'],$seats['reImprecion']);
-		$this->sala          = $seats['sala'];
-		$this->horario       = $seats['horario'];
-		$this->user          = $seats['user'];
-		
-		foreach ($seats['seat'] as $key => $value) {
+			if ($data['backendPrint']) {
+				foreach($data["productos"] as $producto) {
+					
+					$printer->setJustification(Printer::JUSTIFY_LEFT);
+					$printer->text($producto['selectedCantidad']);
+					$printer->text(" " . $producto['descripcion']);
+					$printer->text(" $ " . ( $producto['precio'] * $producto['selectedCantidad'] ) . "\n");
+
+				}
+			} else {
+				//try to print procducos
+				foreach($data['productos'] as $producto) {
+					$printer->setJustification(Printer::JUSTIFY_LEFT);
+					$printer->text($producto['selectedCantidad']);
+					$printer->text(" " . $producto['producto']['descripcion']);
+					//$printer->setJustification(Printer::JUSTIFY_RIGHT);	
+					
+					if ($data["precioSelected"] == "1") {
+						$printer->text(" $ " . ( $producto['producto']['precio'] * $producto['selectedCantidad'] ) . "\n");
+					} else  {
+						$printer->text(" $ " . ( $producto['producto']['precio1'] * $producto['selectedCantidad'] ) . "\n");
+					}
+				}
+			}
+
+			$printer->feed(1);
+			$printer->setTextSize(2, 2);
+			$printer->setEmphasis(true);
+			$printer->text("------------------------". "\n");
+			$printer->setEmphasis(false);
+			$printer->setTextSize(1, 1);
+			$printer->feed(1);
 			
-			$this->seat   	   = $value;
-			$this->codigo      = $seats['boleto'] . $value;
-			$this->typoBoleto  = $seats['precios'][$key]['nombre'];
-			$this->precio      = $seats['precios'][$key]['precio'];
+			//$printer->feed(5);
+			$printer->setJustification(Printer::JUSTIFY_RIGHT);
+			$printer->setEmphasis(false);
+			$printer->setTextSize(2, 1);
+			$printer->text("Descuento:  $" . $data['descuento']. " MXN\n");
+			$printer->text("Total: $" . $data['total']. " MXN \n");
+			
+			$printer->feed(2);
+			$printer->setJustification(Printer::JUSTIFY_CENTER);
+			$printer->setTextSize(1, 1);
+			//$printer->text("15 dias de garantia en defectos de fabrica.\n");
+			$printer->text("Gracia por su compra :D.\n");
+			$printer->feed(2);
 
-			$this->printTicket();
+			//$printer->feed(5);
+			$printer->cut();
+			
+			/* Close printer */
+			$printer->close();
+		} catch (Exception $e) {
+			echo "Couldn't print to this printer: " . $e -> getMessage() . "\n";
 		}
 	}
 
-	static function getFormatDate($fecha, $rePrint) {
+	static function getFormatDate() {
+		$fecha = date('Y-m-d H:i:s');
+		$numeroDia = date('d', strtotime($fecha));
+		$dia = date('l', strtotime($fecha));
+		$mes = date('F', strtotime($fecha));
+		$anio = date('Y', strtotime($fecha));
+		$dias_ES = array("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo");
+		$dias_EN = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
+		$nombredia = str_replace($dias_EN, $dias_ES, $dia);
+		$meses_ES = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+		$meses_EN = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+		$nombreMes = str_replace($meses_EN, $meses_ES, $mes);
 
-		//when do re print ticket backend send us the corrrect format so we need to do some validation to parse or not
-		if (!$rePrint) {
-			$fecha = substr($fecha, 0, 10);
-			$numeroDia = date('d', strtotime($fecha));
-			$dia = date('l', strtotime($fecha));
-			$mes = date('F', strtotime($fecha));
-			$anio = date('Y', strtotime($fecha));
-			$dias_ES = array("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo");
-			$dias_EN = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
-			$nombredia = str_replace($dias_EN, $dias_ES, $dia);
-			$meses_ES = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
-			$meses_EN = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-			$nombreMes = str_replace($meses_EN, $meses_ES, $mes);
-
-			return $nombredia . " " . $numeroDia . " " . $nombreMes . " " . $anio;
-		}  else {
-			return $fecha;
-		}
+		return $nombredia . " " . $numeroDia . " " . $nombreMes . " " . $anio;
+		
 	}
+
 }
 
 //instance a new objetc
@@ -213,9 +151,11 @@ $tikect = new Ticket();
 
 //Receive the RAW post data.
 $content = trim(file_get_contents("php://input"));
+$data = [];
 
-//Attempt to decode the incoming RAW post data from JSON.
-$decoded = json_decode($content, true);
+//Attempt to decode the incoming RAW post data from Array.
+parse_str($content, $data);
+
 
 // Call the function to start the print process
-$tikect->getTicketData($decoded);
+$tikect->printTicket($data);
